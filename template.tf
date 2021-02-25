317afad7866c5869a833636e19d647367dc937f7
taher <taherkhan@live.co.uk>
2021-02-25 19:18:52 +0000

    first commit of files

---
+++ template.tf
@@ -0,0 +1,23 @@
+/*
+why not local? 
+
+- computer might crash / data loss 
+- not good for collab
+
+centralized managemtn 
+
+- use source control like git 
+
+
+*/
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAXZ5XWYVXUNBHMAHI"
+  secret_key = "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"
+}
+
+resource "aws_instance" "my_ec2" {
+    ami = "ami-098828924dc89ea4a"
+    instance_type = "t2.micro" 
+}
